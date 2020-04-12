82ffee924dbfca8b6dba1d1cd2758c0d953795dd
bestariada <bestariada@gmail.com>
2020-04-12 15:05:53 +0800

    synchronized with thesis

---
+++ CompanioNc/CodeFile1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CompanioNc
+{
+    public class BooleanToForegroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string)
+            {
+                bool b;
+                if (bool.TryParse((string)value, out b))
+                {
+                    if (b == true) return Brushes.Red;
+                }
+            }
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BooleanToFontWeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string)
+            {
+                bool b;
+                if (bool.TryParse((string)value, out b))
+                {
+                    if (b == true) return FontWeights.UltraBold;
+                }
+            }
+            return FontWeights.Normal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public struct WindowInfo
+    {
+        public IntPtr hWnd;
+        public string szWindowName;
+    }
+
+    class GetWindow
+    {
+        private string _key = string.Empty;
+        private string _type = string.Empty;
+
+        //枚举所有屏幕上的顶层窗口，并将窗口句柄传送给应用程序定义的回调函数,lpEnumFunc回调函数的指针。
+        [DllImport("user32.dll")]
+        private static extern bool EnumWindows(WNDENUMPROC lpEnumFunc, int lParam);
+
+        //取的子視窗控制
+        [DllImport("user32.dll")]
+        private static extern int EnumChildWindows(IntPtr hWndParent, CallBack lpfn, int lParam);
+
+        //這是user32的功能, 取得視窗名字
+        // 取得控件的文字(Text)
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        internal static extern int GetWindowText(IntPtr hWnd, [Out, MarshalAs(UnmanagedType.LPTStr)] StringBuilder lpString, int nMaxCount);
+
+        // 函數代理
+        private delegate bool WNDENUMPROC(IntPtr hWnd, int lParam);
+        // 回調函數代理  
+        private delegate bool CallBack(int hwnd, int lParam);
+
+        private WindowInfo[] GetAllDesktopWindows()
+        {
+            List<WindowInfo> wndList = new List<WindowInfo>();
+
+            EnumWindows(delegate (IntPtr hWnd, int lParam)
+            {
+                WindowInfo wnd = new WindowInfo();
+                StringBuilder sb = new StringBuilder(256);
+                //get hwnd
+                wnd.hWnd = hWnd;
+                //获取窗口名
+                GetWindowText(hWnd, sb, sb.Capacity);
+                wnd.szWindowName = sb.ToString();
+                //add it into list
+                // the point of selection *******************
+                if (sb.ToString().Contains(_type))
+                {
+                    wndList.Add(wnd);
+                }
+                return true;
+            }, 0);
+            return wndList.ToArray();
+        }
+
+        // pHWD stands for parent Handle
+        private WindowInfo[] GetAllChildWindows(IntPtr pHWD)
+        {
+            List<WindowInfo> chiList = new List<WindowInfo>();
+            EnumChildWindows(pHWD, delegate (int hWnd, int lParam)
+            {
+                WindowInfo chi = new WindowInfo();
+                StringBuilder sb = new StringBuilder(256);
+                //get hwd
+                chi.hWnd = new IntPtr(hWnd);
+                //取得文字
+                int len;
+                len = GetWindowText(new IntPtr(hWnd), sb, sb.Capacity);
+                if (len > 25)
+                {
+                    chi.szWindowName = sb.ToString();
+                    chiList.Add(chi);
+                }
+                return true;
+            }, 0);
+            return chiList.ToArray();
+        }
+
+        public GetWindow(string type)
+        {
+            _type = type;
+            WindowInfo[] a = GetAllDesktopWindows();
+            if (a.Length > 0)
+            {
+                // >0表示有貨, 取第一位就好
+                WindowInfo[] key = GetAllChildWindows(a[0].hWnd);
+                if (key.Length > 0) _key = key[0].szWindowName.ToString();
+            }
+        }
+
+        public string Key
+        {
+            get { return _key; }
+        }
+    }
+
+    public partial class MainWindow
+    {
+        public void Record_error(string er)
+        {
+            ///created on 2020/03/28, transcribed from vb.net
+            ///寫入錯誤訊息
+            ComDataContext dc = new ComDataContext();
+            log_Err newErr = new log_Err()
+            {
+                error_date = DateTime.Now,
+                application_name = System.Reflection.Assembly.GetExecutingAssembly().FullName,
+                machine_name = Dns.GetHostName(),
+                ip_address = Dns.GetHostEntry(Dns.GetHostName()).AddressList.ToString(),
+                userid = "Ethan",
+                error_message = er
+            };
+            dc.log_Err.InsertOnSubmit(newErr);
+            dc.SubmitChanges();
+        }
+
+        public void Record_admin(string op, string des)
+        {
+            ///寫入作業訊息
+            ComDataContext dc = new ComDataContext();
+            log_Adm newLog = new log_Adm()
+            {
+                regdate = DateTime.Now,
+                application_name = System.Reflection.Assembly.GetExecutingAssembly().FullName.Substring(0,49),
+                machine_name = Dns.GetHostName(),
+                ip_address = Dns.GetHostEntry(Dns.GetHostName()).AddressList[0].ToString(),
+                userid = "Ethan",
+                operation_name = op,
+                description = des
+            };
+            dc.log_Adm.InsertOnSubmit(newLog);
+            dc.SubmitChanges();
+        }
+    }
+}
--- CompanioNc/MainWindow.xaml.cs
+++ CompanioNc/MainWindow.xaml.cs
@@ -1,28 +1,184 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CompanioNc
 {
     /// <summary>
+    ///     '20200411 created
+    ///目的有: 1. 監控問診畫面, 紀錄 -done
+    ///        2. 顯示該人的檢驗或其他有用資料  -done
+    ///        3. 可貼上routine template -plausible
+    ///        4. 可貼上檢驗結果 -plausible
+    ///        5. 可copy 雲端    -done
+    ///        6. 可紀錄是否有查雲端, 是否有查關懷名單 -done
+    ///        7. 量表 -plausible
     /// MainWindow.xaml 的互動邏輯
     /// </summary>
     public partial class MainWindow : Window
     {
+        private System.Timers.Timer _timer1;
+        private string strID = string.Empty;
+        private string strUID = string.Empty;
+        private string strSDATE = string.Empty;
+        private string tempID = string.Empty;
+        public const int MOD_ALT = 0x1; //Alt key for hotkey
+        public const int MOD_SHIFT = 0x4; //Alt key for hotkey
+        public const int MOD_CONTROL = 0x2; //Alt key for hotkey
+        public const int MOD_WINKEY = 0x8; //Alt key for hotkey
+        public const int WM_HOTKEY = 0x312;   //Hotkey
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Main_Loaded(object sender, RoutedEventArgs e)
+        {
+            this._timer1 = new System.Timers.Timer();
+            this._timer1.Interval = 500;
+            this._timer1.Elapsed += new System.Timers.ElapsedEventHandler(_TimersTimer_Elapsed);
+            _timer1.Start();
+            Record_admin("Companion Log in", "");
+            Refresh_data();
+            this.Label1.Content = string.Empty;
+            this.Label2.Content = string.Empty;
+        }
+
+        private void _TimersTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                GetWindow g = new GetWindow("問診");
+                // vb.NET里面的vbCr & vbLf 在c# 是\r\n, chr(13) chr(10)
+                // 第二個是身分證字號
+                //tempID = g.Key.Split('\n')[2];
+                tempID = g.Key;
+                //string[] ss = tempID.Split('\n');
+            }
+            catch
+            {
+                tempID = string.Empty;
+            }
+            if (strID == string.Empty)
+            {
+                if (tempID == string.Empty)
+                {
+                    // condition 1, strID = "" => ""                     do nothing
+                    return;
+                }
+                else
+                {
+                    // 檢單查核, 如果分解後數目小於8, 應該就不是正確的
+                    // 20190930似乎有效
+                    if (tempID.Split(' ').Length < 8)
+                    {
+                        //'MessageBox.Show("抓到了")
+                        return;
+                    }
+                    // condition 2, strID = "" => something A            record A, starttime
+                    // 要做很多事情, 分解
+                    // 20190930 有些"問診畫面"的狀態,文字是不一樣的,這樣的話會有錯誤
+                    strID = tempID;
+                    ComDataDataContext dc = new ComDataDataContext();
+                    string[] s = strID.Split(' ');   //0 SDATE, 1 VIST, 2 RMNO, 4 Nr, 7 uid, 8 cname
+                    strUID = s[7].Substring(1, 10);
+                    if (strUID == "A000000000")
+                    {
+                        strID = "";
+                        strUID = "";
+                        strSDATE = "";
+                        return;
+                    }
+                    strSDATE = s[0];
+                    //dc.sp_insert_access(CDate(s[0]), s[1], CInt(s[2]), CInt(s[4]), strUID, s[8], 1);
+                    // do something
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        this.Label1.Content = strUID;
+                        this.Label2.Content = strID;
+                        // 更新資料
+                        Refresh_data();
+                    }));
+                    //' 寫入current_uid
+                    if (System.IO.File.Exists(@"C:\vpn\current_uid.txt"))
+                    {
+                        // 如果有檔案就殺了它
+                        System.IO.File.Delete(@"C:\vpn\current_uid.txt");
+                    }
+                    System.IO.StreamWriter sw = new System.IO.StreamWriter(@"C:\vpn\current_uid.txt");
+                    sw.Write(strUID);
+                    sw.Close();
+                }
+            }
+            else
+            {
+                if (strID == tempID)
+                {
+                    // condition 3, strID = something A => something A   do nothing
+                    return;
+                }
+                else if (tempID == "")
+                {
+                    //' condition 4, strID = something A => ""            record endtime, write into database
+                    //' 做的事情也不少
+                    ComDataDataContext dc = new ComDataDataContext();
+                    string[] s = strID.Split(' ');   //'0 SDATE, 1 VIST, 2 RMNO, 4 Nr, 7 uid, 8 cname
+                    //dc.sp_insert_access(CDate(s[0]), s[1], CInt(s[2]), CInt(s[4]), s[7].Substring(1, 10), s[8], 0);
+                    strID = tempID;
+                    strUID = string.Empty;
+                    strSDATE = string.Empty;
+                    this.Dispatcher.Invoke((Action)(() =>
+                    {
+                        this.Label1.Content = strUID;
+                        this.Label2.Content = strID;
+                    }));
+                    if (System.IO.File.Exists(@"C:\vpn\current_uid.txt"))
+                    {
+                        //' 如果有檔案就殺了它
+                        System.IO.File.Delete(@"C:\vpn\current_uid.txt");
+                    }
+                    //' 清理檢驗資料
+                    //Me.DataGridView1.Visible = False
+                    //Me.DataGridView2.Visible = False
+                    //Me.DataGridView3.Visible = False
+                    //Me.DataGridView4.Visible = False
+                    //Me.DataGridView5.Visible = False
+                    //Me.DataGridView6.Visible = False
+                    //Me.DataGridView7.Visible = False
+                    //Me.DataGridView8.Visible = False
+                }
+                else
+                {
+                    //' condition 5, strID = something A => something B   I don't know if this is possible
+                    //' 有可能嗎? 我不知道
+                    //' 20191001 答案揭曉了,有可能,因為THESIS在畫form時會有A000000000臨時的資料,然後再讀資料庫蓋上,就會出現something A => something B的情況
+                    //' 我採用檢核若A000000000的情形就不要寫入的方式處理
+                    //' 檢單查核, 如果分解後數目小於8, 應該就不是正確的
+                    //'                MessageBox.Show("抓到了! " + vbCrLf + strID + "=>" + tempID)
+                    if (tempID.Split(' ').Length < 8)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void Refresh_data()
+        {
+            ComDataDataContext dc = new ComDataDataContext();
+            DGQuerry.ItemsSource = dc.sp_querytable();
+        }
+
+        private void Main_Closed(object sender, EventArgs e)
+        {
+            Record_admin("Companion Log out", "");
+            _timer1.Stop();
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            Refresh_data();
         }
     }
 }
