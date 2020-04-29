10697fafe2163e3ab93038ab55bf0ecc4b10119c
bestariada <bestariada@gmail.com>
2020-04-30 00:18:51 +0800

    pager success

--- CompanioNc/Test.xaml.cs
+++ CompanioNc/Test.xaml.cs
@@ -1,7 +1,5 @@
-﻿using mshtml;
-using System;
-using System.Diagnostics;
-using System.Timers;
+﻿using HtmlAgilityPack;
+using mshtml;
 using System.Windows;
 
 namespace CompanioNc
@@ -37,7 +35,7 @@ namespace CompanioNc
             //this._timer1.Elapsed += new System.Timers.ElapsedEventHandler(_TimersTimer_Elapsed);
             //_timer1.Start();
 
-            //this.g.Navigate(@"https://medcloud.nhi.gov.tw/imme0008/IMME0008S01.aspx");
+            this.g.Navigate(@"https://medcloud.nhi.gov.tw/imme0008/IMME0008S01.aspx");
 
             //this.FrameLoadComplete += G_FrameLoadComplete;
         }
@@ -89,20 +87,45 @@ namespace CompanioNc
             /// IHTMLElement a = d.getElementById("ContentPlaceHolder1_a_0060");
             /// a.click();
             /// 按排序鍵
-            HTMLDocument d = (HTMLDocument)g.Document;
-            HTMLDocument f = d.frames.item(0).document.body.document;
-            HTMLDocument htmlgvList = f.getElementById("ContentPlaceHolder1_gvList").document;
-            foreach (IHTMLElement th in htmlgvList.getElementsByTagName("th"))
-            {
-                if (th.innerText == "就醫(調劑)日期(住院用藥起日)")
-                {
-                    th.children(0).click();
-                }
-            }
+            //HTMLDocument d = (HTMLDocument)g.Document;
+            //HTMLDocument f = d.frames.item(0).document.body.document;
+            //HTMLDocument htmlgvList = f.getElementById("ContentPlaceHolder1_gvList").document;
+            //foreach (IHTMLElement th in htmlgvList.getElementsByTagName("th"))
+            //{
+            //    if (th.innerText == "就醫(調劑)日期(住院用藥起日)")
+            //    {
+            //        th.children(0).click();
+            //    }
+            //}
             // test1();
             // test2();
 
-            System.Windows.Forms.MessageBox.Show("Test Success!");
+            //System.Windows.Forms.MessageBox.Show("Test Success!");
+
+            // 找出頁數
+            HTMLDocument d = (HTMLDocument)g.Document;
+            HTMLDocument f = d.frames.item(0).document.body.document;
+            //HtmlDocument p = new HtmlDocument();
+            //p.LoadHtml(f.getElementById(@"ctl00$ContentPlaceHolder1$pg_gvList_input").outerHTML);
+            //HtmlNodeCollection o = p.DocumentNode.SelectNodes("//option");
+
+            //System.Windows.Forms.MessageBox.Show(o.Count.ToString());
+            //string id = "ctl00$ContentPlaceHolder1$pg_gvList";
+            //string pa = "2";
+            foreach (IHTMLElement a in f.getElementById("ContentPlaceHolder1_pg_gvList").all)
+            {
+                if (a.innerText == ">")
+                {
+                    a.click();
+                }
+            }
+            //}
+            //string id = "ctl00$ContentPlaceHolder1$a_0030";
+            //string pa = "";
+            //object[] args = new object[2];
+            //args[0] = (object)id;
+            //args[1] = (object)pa;
+            //f.InvokeScript("__doPostBack", args);
         }
 
         //private void test1()
